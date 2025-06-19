13b45df31728144e12e74d43b22cdab94d2a0b0c
Ryan Kline <ryanjkline@protonmail.com>
2025-06-19 18:40:05 -0400

    Update

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,85 +4,120 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
-config.colors = {
-	-- The default text color
-	foreground = "dec0b4",
-	-- The default background color
-	background = "#170f0b",
+config.color_scheme = "Catppuccin Mocha"
 
-	-- Overrides the cell background color when the current cell is occupied by the
-	-- cursor and the cursor style is set to Block
-	cursor_bg = "#dec0b4",
-	-- Overrides the text color when the current cell is occupied by the cursor
-	cursor_fg = "black",
-	-- Specifies the border color of the cursor when the cursor style is set to Block,
-	-- or the color of the vertical or horizontal bar when the cursor style is set to
-	-- Bar or Underline.
-	cursor_border = "#52ad70",
-
-	-- the foreground color of selected text
-	selection_fg = "black",
-	-- the background color of selected text
-	selection_bg = "#fffacd",
-
-	-- The color of the scrollbar "thumb"; the portion that represents the current viewport
-	scrollbar_thumb = "#222222",
-
-	-- The color of the split lines between panes
-	split = "#444444",
-
-	ansi = {
-		"#241914",
-		"#2f211b",
-		"#6f2200",
-		"#5f473b",
-		"#826253",
-		"#ac836f",
-		"#d5ac98",
-		"#ede0da",
-	},
-	brights = {
-		"#c3b8e5",
-		"#d3e192",
-		"#92c3ae",
-		"#c4bddb",
-		"#cf95b4",
-		"#b5bd90",
-		"#efcebf",
-		"#8bbba6",
-	},
-
-	inactive_tab_edge = "#575757",
-}
+config.font = wezterm.font("DejaVuSansM Nerd Font Mono")
+config.font_size = 16
 
 config.window_background_opacity = 0.8
-
-config.window_frame = {
-	-- The font used in the tab bar.
-	-- Roboto Bold is the default; this font is bundled
-	-- with wezterm.
-	-- Whatever font is selected here, it will have the
-	-- main font setting appended to it to pick up any
-	-- fallback fonts you may have used there.
-	-- font = wezterm.font({ family = "Roboto", weight = "Bold" }),
-
-	-- The size of the font in the tab bar.
-	-- Default to 10.0 on Windows but 12.0 on other systems
-	font_size = 14.0,
-
-	-- The overall background color of the tab bar when
-	-- the window is focused
-	active_titlebar_bg = "#333333",
-
-	-- The overall background color of the tab bar when
-	-- the window is not focused
-	inactive_titlebar_bg = "#333333",
-}
+config.window_decorations = "RESIZE"
 
 config.initial_cols = 120
 config.initial_rows = 28
-config.font_size = 18
 
--- Finally, return the configuration to wezterm:
+-- tmux
+config.leader = { key = "t", mods = "CTRL", timeout_milliseconds = 3000 }
+config.keys = {
+	-- New Tab
+	{ mods = "LEADER", key = "t", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	-- Close Tab
+	{
+		mods = "LEADER",
+		key = "x",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	-- SplitHorizontal
+	{
+		mods = "LEADER",
+		key = "\\",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	-- SplitVertical
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	-- Switch between panes
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "LEADER",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	-- Adjust Pane size
+	{
+		mods = "LEADER",
+		key = "LeftArrow",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "RightArrow",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "DownArrow",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "UpArrow",
+		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
+}
+
+for i = 0, 9 do
+	-- leader + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i),
+	})
+end
+
+-- tab bar
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = true
+
+-- tmux status
+wezterm.on("update-right-status", function(window, _)
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+	local prefix = ""
+
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	end
+
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+	end -- arrow color based on if tab is first pane
+
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#b7bdf8" } },
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
+end)
+
 return config
